e65a00952a10b7ab7d853e6806c1fc8acd840f77
Megan <mebloems@microsoft.com>
2023-11-13 14:14:26 +0100

    add send-message.py

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 This application works with voice-to-text, using [OpenAI's 'Whisper](https://github.com/davabase/whisper_real_time/blob/master/transcribe_demo.py), and counts the words for you. If you reach the maximum (25 words) it will give you a notification.
 
 It is also applicable to other spells, such as 'Magic Mouth'.
+
+## Notes
+
+Online there are multiple speech recognition or voice-to-text services available. A quick browse on huggingface.io comes up with [over 10.000 models](https://huggingface.co/models?pipeline_tag=automatic-speech-recognition&sort=trending).
+
+After spending an entire morning trying to set up whisper and failing **I am going the easy route: using cognitive services [Speech SDK](https://learn.microsoft.com/en-us/azure/ai-services/speech-service/speech-sdk).**
---
+++ src/send-message.py
@@ -0,0 +1,49 @@
+# pip install azure-cognitiveservices-speech
+
+# set SPEECH_KEY=your-key
+# set SPEECH_REGION=your-region
+
+# Recognize speech from (default) microphone + cap at 25 words
+import os
+import azure.cognitiveservices.speech as speechsdk
+
+def recognize_from_microphone():
+    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+    speech_config.speech_recognition_language = "en-US"
+
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    print("Speak into your microphone.")
+    print("")
+    recognized_words = []  # To store recognized words
+
+    while len(recognized_words) < 25:  # Cap recognition at 25 words
+        speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+        if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            recognized_text = speech_recognition_result.text
+            words = recognized_text.split()  # Split the recognized text into words
+            recognized_words.extend(words)  # Add the recognized words to the list
+
+            # Check if the total recognized words exceed the limit
+            if len(recognized_words) > 25:
+                recognized_words = recognized_words[:25]  # Cap it at 25 words
+
+            print("Your original message: {}".format(recognized_text))
+        elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+            print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+        elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = speech_recognition_result.cancellation_details
+            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: {}".format(cancellation_details.error_details))
+                print("Did you set the speech resource key and region values?")
+            break  # Exit the loop if canceled
+
+    # Output the final recognized words
+    recognized_text = ' '.join(recognized_words)
+    print("")
+    print("What was actually sent: {}".format(recognized_text))
+
+recognize_from_microphone()
